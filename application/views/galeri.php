5cc4a176fb0b060c0c2e8d28c047444174593706
Lulu Iwana <lulukiwanatul@gmail.com>
2021-06-09 11:31:22 +0700

    bug

--- application/views/galeri.php
+++ application/views/galeri.php
@@ -20,43 +20,61 @@
                 <!-- Page-body start -->
                 <div class="page-body">
 
-                   <div class="row">
-  <div class="box">
-    <div class="thumbnail text-center">
-      <a href="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697
+                    <div class="row">
+                        <div class="box">
+                            <div class="thumbnail text-center">
+                                <a href="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697
 ">
-        <img src="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697" alt="Lights" class="galeri">
-        <div class="caption text-center">
-          <div>Lorem ipsum...</div>
-          <button class="btn btn-primary btn-sm mb-4">Download</button>
-        </div>
-      </a>
-    </div>
-  </div>
-  <div class="box">
-    <div class="thumbnail">
-      <a href="/w3images/nature.jpg">
-        <img class="galeri" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/1200px-VAN_CAT.png
-" alt="Nature" >
-        <div class="caption">
-          <p>Lorem ipsum...</p>
-        </div>
-      </a>
-    </div>
-  </div>
-  <div class="box">
-    <div class="thumbnail">
-      <a href="/w3images/fjords.jpg">
-        <img class="galeri" src="https://images.unsplash.com/photo-1572360620839-a02312482daf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80
+                                    <img src="https://static.toiimg.com/photo/msid-67586673/67586673.jpg?3918697"
+                                        alt="Lights" class="galeri">
+                                    <div class="caption text-center">
+                                        <div>Lorem ipsum...</div>
+                                        <div class="dropdown-primary dropdown open">
+                                            <button class="btn btn-primary dropdown-toggle waves-effect waves-light "
+                                                type="button" id="dropdown-2" data-toggle="dropdown"
+                                                aria-haspopup="true" aria-expanded="false">Primary</button>
+                                            <div class="dropdown-menu" aria-labelledby="dropdown-2"
+                                                data-dropdown-in="fadeIn" data-dropdown-out="fadeOut"
+                                                x-placement="bottom-start"
+                                                style="position: absolute; transform: translate3d(0px, 44px, 0px); top: 0px; left: 0px; will-change: transform;">
+                                                <a class="dropdown-item waves-light waves-effect" href="#">Action</a>
+                                                <a class="dropdown-item waves-light waves-effect" href="#">Another
+                                                    action</a>
+                                                <a class="dropdown-item waves-light waves-effect" href="#">Something
+                                                    else</a>
+                                                <div class="dropdown-divider"></div>
+                                                <a class="dropdown-item waves-light waves-effect" href="#">Something
+                                                    else</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="thumbnail">
+                                <a href="/w3images/nature.jpg">
+                                    <img class="galeri" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/1200px-VAN_CAT.png
+" alt="Nature">
+                                    <div class="caption">
+                                        <p>Lorem ipsum...</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="box">
+                            <div class="thumbnail">
+                                <a href="/w3images/fjords.jpg">
+                                    <img class="galeri" src="https://images.unsplash.com/photo-1572360620839-a02312482daf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80
 
 " alt="Fjords">
-        <div class="caption">
-          <p>Lorem ipsum...</p>
-        </div>
-      </a>
-    </div>
-  </div>
-</div>
+                                    <div class="caption">
+                                        <p>Lorem ipsum...</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
                 <!-- Page-body end -->
